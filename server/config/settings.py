c713af46e0eafe58f34223f617e1f0e4171c8bc0
olajirestephen <horlharmighty2000@gmail.com>
2026-04-02 09:42:46 -0500

    added more features

--- server/config/settings.py
+++ server/config/settings.py
@@ -101,11 +101,20 @@ TEMPLATES = [
 
 
 # ── Database ──
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME":   BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME":   BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=config("DATABASE_URL"),
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True
+    )
 }
 
 
@@ -192,6 +201,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",   # Vite dev server
     "http://127.0.0.1:5173",
     "http://localhost:3000",
+    "https://inventory-7gxd.onrender.com"
 ]
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = [
